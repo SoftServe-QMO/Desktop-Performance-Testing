5471b04718345bd4e09587c9b650b1e57e2797eb
SoftServe-QMO <oelyash@softservecom.com>
2015-07-30 18:08:12 +0300

    Initial commit

---
+++ PerfMonProxy/ClassLibrary1/PerfMonitorProxy.cs
@@ -0,0 +1,30 @@
+﻿using RGiesecke.DllExport;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class PerfMonitorProxy
+{
+    [DllExport("perfmon", CallingConvention = CallingConvention.Cdecl)]
+    public static int TestExport(String counterGroup, String counterSet, int value)
+    {
+        System.Console.Out.WriteLine(String.Format("Setting counter {0} for group {1} with value {2}", counterSet, counterGroup, value));
+
+
+        // get an instance of our perf counter
+        PerformanceCounter counter = new PerformanceCounter();
+        counter.CategoryName = counterGroup;
+        counter.CounterName = counterSet;
+        counter.ReadOnly = false;
+        counter.RawValue = value;            
+        
+        counter.Close();
+        return 0;
+    }
+}
